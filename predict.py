22ed12e1c7467b5a17dc6956c96efa00caacdcf7
Var-01 <vartikaverma011929@gmail.com>
2023-05-07 20:03:05 +0530

    Working on prediction system

--- predict.py
+++ predict.py
@@ -6,6 +6,3 @@ from statsmodels.tsa.arima.model import ARIMA
 df = pd.read_csv('C:\\Users\\anok1\\Documents\\Predict Git\\Prediction-System\\inventory_dataset.csv')
 # print(df)
 
-cookies_data = df[df['Sub Category'] == 'Cookies']
-df = cookies_data
-print(df)
