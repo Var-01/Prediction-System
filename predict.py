93369ccd4aadeafaec6549ddc9d9548fe8a33adb
Var-01 <vartikaverma011929@gmail.com>
2023-05-07 20:04:18 +0530

    Processing dataset

--- predict.py
+++ predict.py
@@ -6,3 +6,11 @@ from statsmodels.tsa.arima.model import ARIMA
 df = pd.read_csv('C:\\Users\\anok1\\Documents\\Predict Git\\Prediction-System\\inventory_dataset.csv')
 # print(df)
 
+cookies_data = df[df['Sub Category'] == 'Cookies']
+df = cookies_data
+# print(df)
+
+df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
+df['Date'] = df['Date'].dt.to_period('M').dt.to_timestamp()
+df = df.groupby('Date')['Sales'].sum().reset_index()
+# print(df)
