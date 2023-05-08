babf19530fad6c8636c3f3fba3fb41a4dae9903e
Var-01 <vartikaverma011929@gmail.com>
2023-05-08 19:06:01 +0530

    Filtering dataset

--- predict.py
+++ predict.py
@@ -14,3 +14,14 @@ df['Date'] = pd.to_datetime(df['Date'], format="%Y-%m-%d")
 df['Date'] = df['Date'].dt.to_period('M').dt.to_timestamp()
 df = df.groupby('Date')['Sales'].sum().reset_index()
 # print(df)
+
+old_column_name = 'Date'
+new_column_name = 'Month'
+df = df.rename(columns={old_column_name: new_column_name})
+
+df.columns=["Month","Sales"]
+df.head()
+df.describe()
+
+df.set_index('Month',inplace=True)
+# print(df)
