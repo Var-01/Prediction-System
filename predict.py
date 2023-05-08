d181eeffcc364d52d8dffcb421e64644b646da64
Var-01 <vartikaverma011929@gmail.com>
2023-05-08 19:08:51 +0530

    making adfuller function

--- predict.py
+++ predict.py
@@ -32,3 +32,16 @@ df.plot()
 
 from statsmodels.tsa.stattools import adfuller
 test_result=adfuller(df['Sales'])
+
+def adfuller_test(sales):
+    result = adfuller(sales)
+    labels = ['ADF Test Statistic', 'p-value', '#Lags Used', 'Number of Observations']
+    for value, label in zip(result, labels):
+        print(label + ' : ' + str(value))
+
+    if result[1] <= 0.05:
+        print("Strong evidence against the null hypothesis (Ho), reject the null hypothesis. Data is stationary.")
+    else:
+        print("Weak evidence against the null hypothesis, indicating it is non-stationary.")
+
+adfuller_test(df['Sales'])
